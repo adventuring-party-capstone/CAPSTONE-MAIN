e5158cb709a2d99582c3a3dae21e213cdaf3cba1
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-09-21 16:56:58 -0400

    seeded data into genres table

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,15 +2,16 @@
 
 const client = require("./client");
 
-const { createUser } = require('./helpers/users');
-const {createDrink} = require('./helpers/drinks');
-const { users, drinks } = require('./seedData')
+const { createUser } = require("./helpers/users");
+const { createDrink } = require("./helpers/drinks");
+const { createGenre } = require("./helpers/genres");
+const { users, drinks, genres } = require("./seedData");
 // Drop Tables for cleanliness
 
 const dropTables = async () => {
-    try {
-        console.log("Starting to drop tables");
-        await client.query(`
+     try {
+          console.log("Starting to drop tables");
+          await client.query(`
         DROP TABLE IF EXISTS users cascade;
         DROP TABLE IF EXISTS drinks cascade;
         DROP TABLE IF EXISTS genres cascade;
@@ -18,18 +19,18 @@ const dropTables = async () => {
         DROP TABLE IF EXISTS ingredients cascade;
         DROP TABLE IF EXISTS genres_ingredients cascade;
         `);
-        console.log("Tables dropped!");
-    } catch (error) {
-        console.log("Error dropping tables");
-        throw error;
-    }
+          console.log("Tables dropped!");
+     } catch (error) {
+          console.log("Error dropping tables");
+          throw error;
+     }
 };
 
 //Create Tables
 const createTables = async () => {
-    console.log("Building tables...");
-    await client.query(
-        `CREATE TABLE users (
+     console.log("Building tables...");
+     await client.query(
+          `CREATE TABLE users (
             users_id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
             password TEXT NOT NULL
@@ -64,58 +65,75 @@ const createTables = async () => {
             ingredients_id INTEGER REFERENCES ingredients(ingredients_id)
         );
         `
-    );
-    console.log("Tables built!");
+     );
+     console.log("Tables built!");
 };
 
 // Create initial users
 const createInitialUsers = async () => {
-    try {
-        console.log('...starting to create initial users')
-        console.log("users", users)
-        for (const user of users) {
-            await createUser(user);
-        }
-        console.log("initial users created");
-        console.log("Users", users)
-    } catch (error) {
-        throw error;
-    }
+     try {
+          console.log("...starting to create initial users");
+          console.log("users", users);
+          for (const user of users) {
+               await createUser(user);
+          }
+          console.log("initial users created");
+          console.log("Users", users);
+     } catch (error) {
+          throw error;
+     }
 };
+
+//creating initial drinks
 const createInitialDrinks = async () => {
-    try {
-        console.log("...starting to create initial drinks")
-        for (const drink of drinks){
-            await createDrink(drink);
-        }
-        console.log("initial drinks created")
-        console.log("Drinks", drinks)
-    } catch (error) {
-        throw error;
-    }
+     try {
+          console.log("...starting to create initial drinks");
+          for (const drink of drinks) {
+               await createDrink(drink);
+          }
+          console.log("initial drinks created");
+          console.log("Drinks", drinks);
+     } catch (error) {
+          throw error;
+     }
+};
+
+//creating initial genres
+const createInitialGenres = async () => {
+     try {
+          console.log("starting to create initial genres...");
+          for (const genre of genres) {
+               await createGenre(genre);
+          }
+          console.log("initial genres created!");
+          console.log("Genres ", genres);
+     } catch (error) {
+          throw error;
+     }
 };
 
 // Call all functions and build database
 const rebuildDb = async () => {
-    try {
-        // connect to local database
-        console.log("entering rebuildDB function");
-        client.connect();
+     try {
+          // connect to local database
+          console.log("entering rebuildDB function");
+          client.connect();
 
-        // run functions
-        await dropTables();
-        await createTables();
+          // run functions
+          await dropTables();
+          await createTables();
 
-        // Generating starting data
-        console.log("starting to seed data...")
-        await createInitialUsers();
-        await createInitialDrinks();
-    } catch (error) {
-        console.error("Can't build DB", error);
-    } finally {
-        // close connection
-        client.end();
-    }
+          // Generating starting data
+          console.log("starting to seed data...");
+          await createInitialUsers();
+          await createInitialDrinks();
+          await createInitialGenres();
+     } catch (error) {
+          console.error("Can't build DB", error);
+     } finally {
+          // close connection
+          client.end();
+     }
 };
 
 rebuildDb();
