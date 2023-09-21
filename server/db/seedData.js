3cd8775959c29d205f89758d069887475ac6e54d
Linda Givers <lindagivers@gmail.com>
2023-09-21 12:26:00 -0400

    created genres and genre ingredients tables and users fav tables

--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,11 +1,12 @@
-//Users
+//seedData for Users
 
 const users = [
-     { users_id: 1, username: "syltbar", password: "syltbar" },
+     { users_id: 1, username: "Asteria", password: "cuteness" },
      { users_id: 2, username: "Yuki", password: "iAmSmol" },
      { users_id: 3, username: "Nacho", password: "iAmBig" },
 ];
 
+//Drinks
 const drinks = [
      {
           drinks_id: 1,
@@ -108,3 +109,120 @@ const drinks = [
           alcoholic: true,
      },
 ];
+
+//Genres
+const genres = [
+     {
+          genres_id: 1,
+          genres_name: "R&B"
+     },
+     {
+          genres_id: 2,
+          genres_name: "Jazz"
+     },
+     {
+          genres_id: 3,
+          genres_name: "Pop"
+     },
+     {
+          genres_id: 4,
+          genres_name: "Rock"
+     },
+     {
+          genres_id: 5,
+          genres_name: "EDM"
+     },
+     {
+          genres_id: 6,
+          genres_name: "Hip Hop"
+     },
+     {
+          genres_id: 7,
+          genres_name: "Classical"
+     },
+     {
+          genres_id: 8,
+          genres_name: "Kpop"
+     },
+     {
+          genres_id: 9,
+          genres_name: "Dance pop"
+     },
+     {
+          genres_id: 10,
+          genres_name: "Alternative Rock"
+     },
+]
+
+//Users drinks favorites
+ const users_drinks_favorites = [
+     {
+          users_drinks_id: 1,
+          users_id: 1,
+          drinks_id: 1
+     },
+     {
+          users_drinks_id: 2,
+          users_id: 2,
+          drinks_id: 2
+     },
+     {
+          users_drinks_id: 3,
+          users_id: 3,
+          drinks_id: 3
+     },
+]
+
+// Tying genres and ingredients
+const genres_ingredients_id = [
+     {
+          genres_ingredients_id: 1,
+          genres_id: 1,
+          ingredients_id: 1
+     },
+     {
+          genres_ingredients_id: 2,
+          genres_id: 2,
+          ingredients_id: 2
+     },
+     {
+          genres_ingredients_id: 3,
+          genres_id: 3,
+          ingredients_id: 3
+     },
+     {
+          genres_ingredients_id: 4,
+          genres_id: 4,
+          ingredients_id: 4
+     },
+     {
+          genres_ingredients_id: 5,
+          genres_id: 5,
+          ingredients_id: 5
+     },
+     {
+          genres_ingredients_id: 6,
+          genres_id: 6,
+          ingredients_id: 6
+     },
+     {
+          genres_ingredients_id: 7,
+          genres_id: 7,
+          ingredients_id: 7
+     },
+     {
+          genres_ingredients_id: 8,
+          genres_id: 8,
+          ingredients_id: 8
+     },
+     {
+          genres_ingredients_id: 9,
+          genres_id: 9,
+          ingredients_id: 9
+     },
+     {
+          genres_ingredients_id: 10,
+          genres_id: 10,
+          ingredients_id: 10
+     },
+]
