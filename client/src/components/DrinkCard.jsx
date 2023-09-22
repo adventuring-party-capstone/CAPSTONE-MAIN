51a067a6041e2978029da9b53df5376dac60308c
winniemei <wmei91@gmail.com>
2023-09-22 18:51:16 -0400

    get drink by ingredient

--- client/src/components/DrinkCard.jsx
+++ client/src/components/DrinkCard.jsx
@@ -3,7 +3,19 @@ import { fetchAllDrinks, fetchAllGenresIngredients } from "../../fetching/local"
 
 
 export default function DrinkCard({ musicChoice }) {
+    const [drinks, setDrinks] = useState([]);
     const [junctionGenresIngredients, setJunctionGenresIngredients] = useState([]);
+
+    // get all drinks
+    useEffect(() => {
+        async function getAllDrinks() {
+            const response = await fetchAllDrinks();
+            console.log(response);
+            setDrinks(response);
+            console.log('ALL DRINKS', response)
+        }
+        getAllDrinks();
+    }, []);
 
     // get all genres_ingredients
 
@@ -34,7 +46,20 @@ export default function DrinkCard({ musicChoice }) {
             <div>
                 <h1>DRINK CARD</h1>
                 <h1>Filtered Ingredient ID {filtered_ingredients}</h1>
+                {drinks
+                    .filter((drink) => filtered_ingredients.includes(drink.drinks_id))
+                    .map((drink) => {
+                        return (
+                            <>
+                                <div id="each-drink">
+                                    <h3>{drink.drinks_name}</h3>
+                                </div>
+                            </>
+                        )
+                    }
+                    )}
             </div>
         </>
     )
+
 }
