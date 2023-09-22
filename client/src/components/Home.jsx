4ac94ca15259a790e4c52dec26afe22c3262aafe
Emily Cheng <echeng0123@gmail.com>
2023-09-22 14:27:30 -0400

    pseudocoded homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,3 +7,17 @@ export default function Home() {
 		</div>
 	);
 }
+
+// need a search bar
+// when someone searches genre id
+// save that search param in a variable
+// look inside genres_ingredients junction table
+// get an array of ingredients matching that genre
+
+// API: get all drinks with ingredient
+// localdb: get all drinks with ingredient matching ingredient_id in genres_ingredients junction table
+
+// render a single drink from the array of drinks matching ingredient on button click,
+// when someone clicks again it goes to the next drink
+
+// watch out! for duplicate drinks with external API
