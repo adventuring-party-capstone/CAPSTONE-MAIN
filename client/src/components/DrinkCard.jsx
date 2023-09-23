3523c2a2873bb12207f44eb94c7707e11c817a49
winniemei <wmei91@gmail.com>
2023-09-22 20:09:43 -0400

    did junction table name matching

--- client/src/components/DrinkCard.jsx
+++ client/src/components/DrinkCard.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { fetchAllDrinks, fetchAllGenresIngredients } from "../../fetching/local";
+import { fetchAllDrinks, fetchAllGenresIngredients, fetchAllIngredients } from "../../fetching/local";
 
 
 export default function DrinkCard({ musicChoice }) {
     const [drinks, setDrinks] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
     const [junctionGenresIngredients, setJunctionGenresIngredients] = useState([]);
 
     // get all drinks
@@ -27,15 +28,36 @@ export default function DrinkCard({ musicChoice }) {
         getAllGenresIngredients();
     }, []);
 
+    // get all ingredients
+    useEffect(() => {
+        async function getAllIngredients() {
+            const ingredients = await fetchAllIngredients();
+            setIngredients(ingredients);
+            console.log('ingredients', ingredients);
+        }
+        getAllIngredients();
+    }, []);
+
     // getting associated corresponding ingredients_id from genres_id in junctionGenresIngredients
-    const filtered_ingredients = [];
+    const filtered_ingredients_ids = [];
+    const filtered_ingredients_names = [];
 
     {
         junctionGenresIngredients.map((junctionGenreIngredient) => {
             console.log('LOGGING FILTERED INGREDIENTS...')
             if (junctionGenreIngredient.genres_id == musicChoice) {
-                filtered_ingredients.push(junctionGenreIngredient.ingredients_id);
-                console.log("FILTERED INGREDIENTS", filtered_ingredients)
+                // getting the ingredients_ids associated with genres_id
+                filtered_ingredients_ids.push(junctionGenreIngredient.ingredients_id);
+                console.log("FILTERED INGREDIENTS", filtered_ingredients_ids)
+
+                // getting the ingredients_name associated with ingredients_id
+                ingredients.map((ingredient) => {
+                    if (filtered_ingredients_ids.includes(ingredient.ingredients_id)) {
+                        filtered_ingredients_names.push(ingredient.ingredients_name);
+                        console.log("FILTERED INGREDIENTS NAMES", filtered_ingredients_names);
+                    }
+
+                })
             }
         })
 
@@ -44,9 +66,9 @@ export default function DrinkCard({ musicChoice }) {
         <>
             <div>
                 <h1>DRINK CARD</h1>
-                <h1>Filtered Ingredient ID {filtered_ingredients}</h1>
+                <h1>INGREDIENT {filtered_ingredients_names}</h1>
                 {drinks
-                    .filter((drink) => filtered_ingredients.includes(drink.drinks_id))
+                    .filter((drink) => filtered_ingredients_names.includes(drink.ingredients))
                     .map((drink) => {
                         return (
                             <>
