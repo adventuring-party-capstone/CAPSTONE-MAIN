2ef68cafb3f224b8d8c5c23b296946d7a573e04f
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-09-26 16:29:52 -0400

    passed token and setToken props from app down to home, navbar, mainSection; conditional rendering on navBar works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 import "./App.css";
 import MainSection from "./components/MainSection";
 import NavBar from "./components/NavBar";
 
 function App() {
-	// const [count, setCount] = useState(0)
+     //get tokens where other components can use them
+     const [token, setToken] = useState(null);
 
-	return (
-		<div id="app-container">
-			<h1>let;s get this money</h1>
-			<NavBar />
-			<MainSection />
-		</div>
-	);
+     return (
+          <div id="app-container">
+               <h1>let;s get this money</h1>
+               <NavBar token={token} setToken={setToken} />
+               <MainSection token={token} setToken={setToken} />
+          </div>
+     );
 }
 
 export default App;
