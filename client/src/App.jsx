4fc49fe56a4333f2c35c83fb8805341c521fb9a6
Emily Cheng <echeng0123@gmail.com>
2023-09-27 11:38:20 -0400

    correctly showing only one user's favorites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,20 @@ import MainSection from "./components/MainSection";
 import NavBar from "./components/NavBar";
 
 function App() {
-     //get tokens where other components can use them
-     const [token, setToken] = useState(null);
-     const [userId, setUserId] = useState(null);
-     useEffect(() => {
-          setToken(window.localStorage.getItem("token"));
-          setUserId(window.localStorage.getItem("userId"));
-     }, [])
-     return (
-          <div id="app-container">
-               <h1>let;s get this money</h1>
-               <NavBar token={token} setToken={setToken} />
-               <MainSection token={token} setToken={setToken} />
-          </div>
-     );
+	//get tokens where other components can use them
+	const [token, setToken] = useState(null);
+	const [userId, setUserId] = useState(null);
+	useEffect(() => {
+		setToken(window.localStorage.getItem("token"));
+		setUserId(window.localStorage.getItem("userId"));
+	}, []);
+	return (
+		<div id="app-container">
+			<h1>let;s get this money</h1>
+			<NavBar token={token} setToken={setToken} userId={userId} />
+			<MainSection token={token} setToken={setToken} userId={userId} />
+		</div>
+	);
 }
 
 export default App;
