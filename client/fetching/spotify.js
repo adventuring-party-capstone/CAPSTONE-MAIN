4901dd75eeb33f1a0d348e15da14084368e6dd9c
Emily Cheng <echeng0123@gmail.com>
2023-09-29 15:00:06 -0400

    able to get genre id from spotify connected to local genres

--- client/fetching/spotify.js
+++ client/fetching/spotify.js
@@ -1,30 +1,27 @@
 const base_url = "https://api.spotify.com/v1/search?q=";
-const spotifyToken = "BQD6zKgZG6xQlYRVsbxbcUIjfbwIEhS2HVJcYIiDtOcaOroFJ4TxI8WWJVFQdtfb4iKOH7DYCTIJ1_Ep6YTy1lkutZSTvcB_sRYd7rlkqpFrEMkEzes";
+const spotifyToken =
+	"BQD6zKgZG6xQlYRVsbxbcUIjfbwIEhS2HVJcYIiDtOcaOroFJ4TxI8WWJVFQdtfb4iKOH7DYCTIJ1_Ep6YTy1lkutZSTvcB_sRYd7rlkqpFrEMkEzes";
 
 // grabs artist object from spotify search endpoint
 export const fetchArtistSearch = async (artistInput) => {
-    try {
-        console.log("...starting to fetch artist");
-        const response = await fetch(`${base_url}${artistInput}&type=artist`, {
-            method: "GET",
-            headers: {
-                // "Content-Type": "application/json",
-                "Authorization": `Bearer ${spotifyToken}`
-            }
-        });
-        const result = await response.json();
-        console.log("result", result);
-        // console.log("result.artists.items", result.artists.items);
-        const genreArray = result.artists.items[0].genres;
-        console.log("genreArray", genreArray);
-        const foundGenre = genreArray.find((ele) =>
-            ele.includes('r')
-        );
-        console.log("foundGenre", foundGenre);
-        return genreArray;
-    } catch (error) {
-        console.error("Cannot fetch artistSearch", error);
-    }
+	try {
+		console.log("...starting to fetch artist");
+		const response = await fetch(`${base_url}${artistInput}&type=artist`, {
+			method: "GET",
+			headers: {
+				// "Content-Type": "application/json",
+				Authorization: `Bearer ${spotifyToken}`,
+			},
+		});
+		const result = await response.json();
+		// console.log("result", result);
+		// console.log("result.artists.items", result.artists.items);
+		const genreArray = result.artists.items[0].genres;
+		console.log("genreArray", genreArray);
+		return genreArray;
+	} catch (error) {
+		console.error("Cannot fetch artistSearch", error);
+	}
 };
 
 // return genreArray from fetchArtistSearch
@@ -36,9 +33,6 @@ export const fetchArtistSearch = async (artistInput) => {
 // compare genreArray with genresSetState from component
 // the first genre from genreArray that matches the first genre from genreSetState (use find or filter)
 // that will be the genre we want to return drinks for
-
-
-
 
 // in component, fetchAllGenres
 // set a setState for result from fetchAllGenres
--- client/src/components/spotifyTesting.js
+++ client/src/components/spotifyTesting.js
@@ -2,10 +2,8 @@ import { fetchArtistSearch } from "../../fetching/spotify.js";
 
 import { fetchAllGenres } from "../../fetching/local.js";
 
-
 const localDatabaseGenre = fetchAllGenres();
 console.log("localDatabaseGenre", localDatabaseGenre);
 
 const artistGenre = fetchArtistSearch("beyonce");
 console.log("artistGenre", artistGenre);
-
