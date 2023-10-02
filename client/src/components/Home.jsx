ed7381da4a3f782dedb87a8fe248110291eb76dd
winniemei <wmei91@gmail.com>
2023-10-02 14:34:12 -0400

    enable 2 word artist

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,59 +9,70 @@ import { Link, useNavigate } from "react-router-dom";
 import CocktailDBDrinkCard from "./CocktailDBDrinkCard";
 
 export default function Home({ token, setToken, userId, setUserId }) {
-	const [musicInput, setMusicInput] = useState("");
-	const [musicChoice, setMusicChoice] = useState("");
-	const [isClicked, setIsClicked] = useState(false);
+     const [musicInput, setMusicInput] = useState("");
+     const [musicChoice, setMusicChoice] = useState("");
+     const [isClicked, setIsClicked] = useState(false);
 
-	// const navigate = useNavigate();
+     // const navigate = useNavigate();
 
-	console.log("userId in home: ", userId);
+     console.log("userId in home: ", userId);
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		setMusicChoice(musicInput);
-		// console.log("musicChoice", musicChoice);
+     const handleSubmit = async (event) => {
+          event.preventDefault();
+          const realMusicInput = handleInput(musicInput);
+          console.log("real music input", realMusicInput);
+          setMusicChoice(realMusicInput);
+          // console.log("musicChoice", musicChoice);
 
-		if (musicChoice) {
-			console.log("entering handleSubmit");
-			setIsClicked(!isClicked);
-			// navigate(0);
-			// setMusicChoice("");
-		} else {
-			console.log("can't get drink choice");
-		}
-	};
+          if (musicChoice) {
+               console.log("entering handleSubmit");
+               setIsClicked(!isClicked);
+               // navigate(0);
+               // setMusicChoice("");
+          } else {
+               console.log("can't get drink choice");
+          }
+     };
 
-	return (
-		<div id="home-container">
-			<h1>ENTER MUSIC CHOICE</h1>
-			<form action="">
-				<label htmlFor="Search">Enter Genre </label>
-				<input
-					type="text"
-					name="search"
-					placeholder="Enter music choice here"
-					onChange={(event) => setMusicInput(event.target.value)}
-					// onKeyDown={handleKeyDown}
-				/>
-			</form>
-			<br />
-			<button onClick={(event) => handleSubmit(event)}>Suggest Drink</button>
-			{/* <CocktailDBDrinkCard /> */}
-			{musicChoice && <Spotify musicChoice={musicChoice} userId={userId} />}
-			{!token && (
-				<div>
-					<Login
-						token={token}
-						setToken={setToken}
-						setUserId={setUserId}
-						userId={userId}
-					/>
-					<Link to={"/register"}>Create New Account</Link>
-				</div>
-			)}
-		</div>
-	);
+     const handleInput = (musicInput) => {
+          // if musicInput has space, then replace the space with a plus symbol
+          // else return music input
+          if (/\s/.test(musicInput)) {
+               return musicInput.replace(" ", "+");
+          } else {
+               return musicInput;
+          }
+     }
+     return (
+          <div id="home-container">
+               <h1>ENTER MUSIC CHOICE</h1>
+               <form action="">
+                    <label htmlFor="Search">Enter Genre </label>
+                    <input
+                         type="text"
+                         name="search"
+                         placeholder="Enter music choice here"
+                         onChange={(event) => setMusicInput(event.target.value)}
+                    // onKeyDown={handleKeyDown}
+                    />
+               </form>
+               <br />
+               <button onClick={(event) => handleSubmit(event)}>Suggest Drink</button>
+               {/* <CocktailDBDrinkCard /> */}
+               {musicChoice && <Spotify musicChoice={musicChoice} userId={userId} />}
+               {!token && (
+                    <div>
+                         <Login
+                              token={token}
+                              setToken={setToken}
+                              setUserId={setUserId}
+                              userId={userId}
+                         />
+                         <Link to={"/register"}>Create New Account</Link>
+                    </div>
+               )}
+          </div>
+     );
 }
 
 // need a search bar
