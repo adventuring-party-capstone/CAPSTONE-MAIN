98d2083f7b659f4626753ed261ca7b191fd79d86
Emily Cheng <echeng0123@gmail.com>
2023-10-02 16:46:03 -0400

    working details button on homepage

--- client/src/components/CocktailDBDrinkCard.jsx
+++ client/src/components/CocktailDBDrinkCard.jsx
@@ -3,8 +3,9 @@ import {
 	fetchAllAlcDrinks,
 	fetchAllNonAlcDrinks,
 	fetchCocktailsByIngredient,
-     fetchCocktailById
+	fetchCocktailById,
 } from "../../fetching/cocktaildb";
+import DetailsButton from "./DetailsButton";
 
 export default function CocktailDBDrinkCard({ ingredientName }) {
 	const [alcDrinks, setAlcDrinks] = useState([]);
@@ -12,7 +13,7 @@ export default function CocktailDBDrinkCard({ ingredientName }) {
 	const [byIngredient, setByIngredient] = useState([]);
 	// const [ingredName, setIngredName] = useState("");
 	const [alcArrayToShow, setAlcArrayToShow] = useState([]);
-     const [drinkToRender, setDrinkToRender] = useState(null);
+	const [drinkToRender, setDrinkToRender] = useState(null);
 
 	const ingredName = ingredientName;
 
@@ -110,14 +111,13 @@ export default function CocktailDBDrinkCard({ ingredientName }) {
 			}
 		}
 		console.log("alcIngredientArray", alcIngredientArray);
-          setDrinkToRender(alcIngredientArray[0]);
-          console.log("Drink to render", drinkToRender);
+		setDrinkToRender(alcIngredientArray[0]);
+		console.log("Drink to render", drinkToRender);
 	}, [alcDrinks, byIngredient]);
 
-     //Take a number from nonAlc or alc ingredient arrays
-     //Pass that number into getDrinkById
-     //To display chosen drink by id
-
+	//Take a number from nonAlc or alc ingredient arrays
+	//Pass that number into getDrinkById
+	//To display chosen drink by id
 
 	return (
 		<section>
@@ -130,8 +130,7 @@ export default function CocktailDBDrinkCard({ ingredientName }) {
 						// eslint-disable-next-line react/jsx-key
 						<section>
 							<p>{drink}</p>
-
-							{/* <SingleDrinkDetails drink={drink} /> */}
+							<DetailsButton drink={drink} />
 						</section>
 					);
 				})
