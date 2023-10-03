3fef36853f6ef8a9d007489593bee3b42b7dc554
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-10-03 13:56:13 -0400

    nonworking toggle on cocktailDBdrinkcard

--- client/src/components/CocktailDBDrinkCard.jsx
+++ client/src/components/CocktailDBDrinkCard.jsx
@@ -1,146 +1,201 @@
 import { useState, useEffect } from "react";
 import {
-	fetchAllAlcDrinks,
-	fetchAllNonAlcDrinks,
-	fetchCocktailsByIngredient,
-	fetchCocktailById,
+     fetchAllAlcDrinks,
+     fetchAllNonAlcDrinks,
+     fetchCocktailsByIngredient,
+     fetchCocktailById,
 } from "../../fetching/cocktaildb";
 import DetailsButton from "./DetailsButton";
+import FavoriteButton from "./FavoriteButton.jsx";
+import * as React from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import { alpha, styled } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
 
 export default function CocktailDBDrinkCard({ ingredientName }) {
-	const [alcDrinks, setAlcDrinks] = useState([]);
-	const [nonAlcDrinks, setNonAlcDrinks] = useState([]);
-	const [byIngredient, setByIngredient] = useState([]);
-	// const [ingredName, setIngredName] = useState("");
-	const [alcArrayToShow, setAlcArrayToShow] = useState([]);
-	const [chosenDrinkId, setChosenDrinkId] = useState(null);
-	const [drinkToRender, setDrinkToRender] = useState([]);
+     const [alcDrinks, setAlcDrinks] = useState([]);
+     const [nonAlcDrinks, setNonAlcDrinks] = useState([]);
+     const [byIngredient, setByIngredient] = useState([]);
+     // const [ingredName, setIngredName] = useState("");
+     //  const [alcArrayToShow, setAlcArrayToShow] = useState([]);
+     const [chosenDrinkId, setChosenDrinkId] = useState(null);
+     const [drinkToRender, setDrinkToRender] = useState([]);
+     const [localArray, setLocalArray] = useState([]);
+     const [isToggled, setIsToggled] = useState(false);
 
-	const ingredName = ingredientName;
-	const chosenId = chosenDrinkId;
+     const ingredName = ingredientName;
 
-	console.log("ingredient name at top", ingredientName);
+     // TOGGLE LOGIC
+     function handleSwitch(event) {
+          setIsToggled(event.target.checked);
+     }
 
-	useEffect(() => {
-		async function getAllAlcDrinks() {
-			const response = await fetchAllAlcDrinks();
-			// console.log("response in getAllAlcDrinks on CDB Drink Card ", response);
-			setAlcDrinks(response.drinks);
-		}
-		getAllAlcDrinks();
-		// console.log("alc drinks in the useEffect", alcDrinks);
-		//COMMENTED OUT FOR SIMPLICITY OF TESTING
-	}, []);
+     // Alcohol toggle
+     const PinkSwitch = styled(Switch)(({ theme }) => ({
+          "& .MuiSwitch-switchBase.Mui-checked": {
+               color: pink[600],
+               "&:hover": {
+                    backgroundColor: alpha(
+                         pink[600],
+                         theme.palette.action.hoverOpacity
+                    ),
+               },
+          },
+          "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+               backgroundColor: pink[600],
+          },
+     }));
 
-	useEffect(() => {
-		async function getAllNonAlcDrinks() {
-			const response = await fetchAllNonAlcDrinks();
+     console.log("ingredient name at top", ingredientName);
 
-			setNonAlcDrinks(response.drinks);
-		}
-		getAllNonAlcDrinks();
-	}, []);
-	//console.log("All nonAlcDrinks in API", nonAlcDrinks);
+     useEffect(() => {
+          async function getAllAlcDrinks() {
+               const response = await fetchAllAlcDrinks();
+               // console.log("response in getAllAlcDrinks on CDB Drink Card ", response);
+               setAlcDrinks(response.drinks);
+          }
+          getAllAlcDrinks();
+          // console.log("alc drinks in the useEffect", alcDrinks);
+          //COMMENTED OUT FOR SIMPLICITY OF TESTING
+     }, []);
 
-	useEffect(() => {
-		console.log("ingredientName in UE", ingredientName);
-		// setIngredName(ingredientName);
+     useEffect(() => {
+          async function getAllNonAlcDrinks() {
+               const response = await fetchAllNonAlcDrinks();
 
-		console.log("ingredNamein UE", ingredName);
-		setTimeout(1000);
-		async function getCocktailsByIngredient() {
-			const response = await fetchCocktailsByIngredient(ingredName);
-			// console.log("all bourbon drinks in API", response.drinks);
-			setByIngredient(response.drinks);
-		}
-		getCocktailsByIngredient();
-		console.log("byIngredient after useEffect", byIngredient);
-	}, [ingredName]);
+               setNonAlcDrinks(response.drinks);
+          }
+          getAllNonAlcDrinks();
+     }, []);
+     //console.log("All nonAlcDrinks in API", nonAlcDrinks);
 
-	//Make an array of idDrink values from all NonAlc
-	//Make an array of idDrink values from Milk
-	//Compare those arratys to see which nonAlcs have Milk
+     useEffect(() => {
+          console.log("ingredientName in UE", ingredientName);
+          // setIngredName(ingredientName);
 
-	const nonAlcArray = [];
+          console.log("ingredNamein UE", ingredName);
+          setTimeout(1000);
+          async function getCocktailsByIngredient() {
+               const response = await fetchCocktailsByIngredient(ingredName);
+               // console.log("all bourbon drinks in API", response.drinks);
+               setByIngredient(response.drinks);
+          }
+          getCocktailsByIngredient();
+          console.log("byIngredient after useEffect", byIngredient);
+     }, [ingredName]);
 
-	//console.log("nonAlcArray ", nonAlcArray);
+     //Make an array of idDrink values from all NonAlc
+     //Make an array of idDrink values from Milk
+     //Compare those arratys to see which nonAlcs have Milk
 
-	const ingredientDrinksAPI = [];
-	const NonAlcIngredientArray = [];
-	useEffect(() => {
-		nonAlcDrinks.map((nonAlcDrink) => {
-			nonAlcArray.push(nonAlcDrink.idDrink);
-		});
-		byIngredient.map((ingredientDrink) => {
-			ingredientDrinksAPI.push(ingredientDrink.idDrink);
-		});
+     const nonAlcArray = [];
 
-		for (let i = 0; i <= nonAlcArray.length; i++) {
-			if (ingredientDrinksAPI.includes(nonAlcArray[i])) {
-				NonAlcIngredientArray.push(nonAlcArray[i]);
-			}
-		}
-		// console.log("ingredientDrinkAPI", ingredientDrinksAPI);
-	}, [byIngredient, nonAlcArray]);
+     //console.log("nonAlcArray ", nonAlcArray);
 
-	//console.log("NonAlcIngredientArray", NonAlcIngredientArray);
+     const ingredientDrinksAPI = [];
+     const NonAlcIngredientArray = [];
+     useEffect(() => {
+          nonAlcDrinks.map((nonAlcDrink) => {
+               nonAlcArray.push(nonAlcDrink.idDrink);
+          });
+          byIngredient.map((ingredientDrink) => {
+               ingredientDrinksAPI.push(ingredientDrink.idDrink);
+          });
 
-	//Make an array of idDrink values from all Alc
-	//Use the ingredient array we already made
-	//Compare those arrays to see which Alcs have Milk
+          for (let i = 0; i <= nonAlcArray.length; i++) {
+               if (ingredientDrinksAPI.includes(nonAlcArray[i])) {
+                    NonAlcIngredientArray.push(nonAlcArray[i]);
+               }
+          }
+          // console.log("ingredientDrinkAPI", ingredientDrinksAPI);
+     }, [byIngredient, nonAlcArray]);
 
-	const alcArray = [];
-	const alcIngredientArray = [];
-	useEffect(() => {
-		alcDrinks.map((alcDrink) => {
-			alcArray.push(alcDrink.idDrink);
-		});
-		// console.log("alcArray", alcArray);
-		byIngredient.map((ingredientDrink) => {
-			ingredientDrinksAPI.push(ingredientDrink.idDrink);
-		});
-		for (let i = 0; i <= alcArray.length; i++) {
-			if (ingredientDrinksAPI.includes(alcArray[i])) {
-				alcIngredientArray.push(alcArray[i]);
-				setAlcArrayToShow(alcIngredientArray);
-			}
-		}
-		console.log("alcIngredientArray", alcIngredientArray);
-		setChosenDrinkId(alcIngredientArray[0]);
-		console.log("Drink to render", chosenDrinkId);
-	}, [alcDrinks, byIngredient]);
+     //console.log("NonAlcIngredientArray", NonAlcIngredientArray);
 
-	// console.log("chosenId outside the useEffect", chosenId);
+     //Make an array of idDrink values from all Alc
+     //Use the ingredient array we already made
+     //Compare those arrays to see which Alcs have Milk
 
-	//currently, this useEffect fires when chosenId changes
-	//chosenId is set inside the function that sets AlcIngredientArray
-	//could we also set chosenId inside the nonAlcIngredientArray? that way, depending on which array complex happens, a different id gets chosen
-	//how do we iterate through the indices when we choose a new drink?
+     const alcArray = [];
+     const alcIngredientArray = [];
+     useEffect(() => {
+          alcDrinks.map((alcDrink) => {
+               alcArray.push(alcDrink.idDrink);
+               //strips down to [id numbers of alc drinks]
+          });
+          // console.log("alcArray", alcArray);
+          byIngredient.map((ingredientDrink) => {
+               ingredientDrinksAPI.push(ingredientDrink.idDrink);
+               //strips down to [id numbers of drinks w/ ingredient]
+          });
+          for (let i = 0; i <= alcArray.length; i++) {
+               if (ingredientDrinksAPI.includes(alcArray[i])) {
+                    alcIngredientArray.push(alcArray[i]);
+                    //[compares the ids between the two]
+               }
+          }
+          console.log("alcIngredientArray", alcIngredientArray);
+          //alcoholic drinks that include the chosen ingredient
 
-	useEffect(() => {
-		console.log("entering cocktail by id");
-		console.log("chosenId in the get by id useEffect", chosenId);
-		// setTimeout(1000);
-		async function getCocktailById() {
-			const response = await fetchCocktailById(chosenId);
-			console.log("response from getCocktailById", response);
-			if (response) {
-				setDrinkToRender(response.drinks[0]);
-			} else {
-				console.log("can't get single drink to render");
-			}
-		}
-		getCocktailById();
-	}, [chosenId]);
+          //concat with the nonalc drinks that include the ingredient
+     }, [alcDrinks, byIngredient]);
 
-	console.log("drink to render above return", drinkToRender);
-	return (
-		<section>
-			<h1>HELLO</h1>
-			{/* {console.log("alcArrayToShow below Hello", alcArrayToShow)} */}
-			{drinkToRender && <h1>{drinkToRender.strDrink}</h1>}
-			{drinkToRender && <img src={drinkToRender.strDrinkThumb} />}
-			{/* {alcArrayToShow ? (
+     //concatenate the alcDrinkIngredient and nonAlcDrinkIngredient Arrays
+     //conditionally setChosenDrinkId based on whether it's from non or concat array
+     useEffect(() => {
+          const toggleOnArray = alcIngredientArray.concat(
+               NonAlcIngredientArray
+          );
+          console.log("toggleOnArray above if/then", toggleOnArray);
+          if (isToggled) {
+               setChosenDrinkId(toggleOnArray[0]);
+               console.log(
+                    "we are in the toggled side and here's the array:",
+                    toggleOnArray
+               );
+          } else if (!isToggled) {
+               setChosenDrinkId(NonAlcIngredientArray[0]);
+          }
+     }, [alcIngredientArray, NonAlcIngredientArray, isToggled]);
+
+     useEffect(() => {
+          console.log("entering fetch cocktail by id");
+          const chosenId = chosenDrinkId;
+          console.log("chosenId in the get by id useEffect", chosenId);
+          // setTimeout(1000);
+          async function getCocktailById() {
+               const response = await fetchCocktailById(chosenId);
+               console.log("response from getCocktailById", response);
+               if (response) {
+                    setDrinkToRender(response.drinks[0]);
+               } else {
+                    console.log("can't get single drink to render");
+               }
+          }
+          getCocktailById();
+     }, [chosenDrinkId, isToggled]);
+
+     console.log("drink to render above return", drinkToRender);
+     return (
+          <section>
+               <FormGroup>
+                    <FormControlLabel
+                         control={
+                              <PinkSwitch
+                                   checked={isToggled}
+                                   onChange={(event) => handleSwitch(event)}
+                              />
+                         }
+                         label="Show alcoholic drinks"
+                    />
+               </FormGroup>
+               <h1>HELLO</h1>
+               {/* {console.log("alcArrayToShow below Hello", alcArrayToShow)} */}
+               {drinkToRender && <h1>{drinkToRender.strDrink}</h1>}
+               {drinkToRender && <img src={drinkToRender.strDrinkThumb} />}
+               {/* {alcArrayToShow ? (
 				alcArrayToShow.map((drink) => {
 					console.log("mapping");
 					return (
@@ -156,6 +211,6 @@ export default function CocktailDBDrinkCard({ ingredientName }) {
 					<h1>OH NO!!!</h1>
 				</>
 			)} */}
-		</section>
-	);
+          </section>
+     );
 }
