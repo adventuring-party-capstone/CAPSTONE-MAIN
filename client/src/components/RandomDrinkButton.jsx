6c53fd8a6028278a3fd7483945706a07df41727c
shaberry <jaden.png02@gmail.com>
2023-10-04 13:02:41 -0700

    working random drink button

---
+++ client/src/components/RandomDrinkButton.jsx
@@ -0,0 +1,43 @@
+import { fetchRandomDrink } from "../../fetching/randomdrink";
+import RandomDrink from "./RandomDrink";
+
+// export default function RandomDrinkButton() {
+//   async function newRandomDrink(event) {
+//     event.preventDefault();
+//     try {
+//       await RandomDrink();
+//     } catch (error) {
+//       console.error("well ya darn messed up", error);
+//     }
+//   }
+//   return (
+//     <>
+//       <div className="random">{RandomDrink.strId}</div>
+//       <div className="Random-Button">
+//         <button onClick={newRandomDrink}>Random Drink! :3</button>
+//       </div>
+//     </>
+//   );
+// };
+export default function RandomDrinkButton() {
+  async function newRandomDrink() {
+    // this works and grabs the object info!! V
+    try {
+      await RandomDrink();
+    //   RandomDrink();
+    } catch (error) {
+      console.error("well ya darn messed up", error);
+    }
+  }
+  newRandomDrink();
+  //   KEEP THIS
+  return (
+    <>
+      {/* Button */}
+      <div className="random">{RandomDrink.strId}</div>
+      <div className="Random-Button">
+        <button onClick={fetchRandomDrink}>Random Drink! :3</button>
+      </div>
+    </>
+  );
+};
