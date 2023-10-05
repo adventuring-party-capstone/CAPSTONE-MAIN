2c1efe8c30638b1b01919599b89fe06537bb4ef3
Emily Cheng <echeng0123@gmail.com>
2023-10-05 19:40:20 -0400

    working darkmode all in app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,40 +3,66 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import MainSection from "./components/MainSection";
 import NavBar from "./components/NavBar";
-import DarkModeToggle from "./components/DarkModeToggle";
+// import DarkModeToggle from "./components/DarkModeToggle";
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
+// import { useColorScheme } from "./useColorScheme";
 
 function App() {
 	//get tokens where other components can use them
 	const [spotifyToken, setSpotifyToken] = useState(null);
 	const [token, setToken] = useState(null);
 	const [userId, setUserId] = useState(null);
-	const [appClass, setAppClass] = useState("");
-	const [eleId, setEleId] = useState("");
-	const [isDark, setIsDark] = useState(
-		localStorage.getItem("isDark") === "true"
-	);
+	// const [appClass, setAppClass] = useState("");
+	// const [eleId, setEleId] = useState("");
+	const [isDark, setIsDark] = useState(true);
 
 	useEffect(() => {
 		setToken(window.localStorage.getItem("token"));
 		setUserId(window.localStorage.getItem("userId"));
 		setSpotifyToken(window.localStorage.getItem("spotifyToken"));
-		setIsDark(window.localStorage.getItem("isDark"));
+		setIsDark(window.localStorage.getItem("dark-mode"));
 	}, []);
 
+	// DARK MODE CODE
+	let darkMode = localStorage.getItem("dark-mode");
+
 	console.log("isDark in App", isDark);
-	console.log(
-		"windowLocalStorage in App",
-		window.localStorage.getItem("isDark")
-	);
+	// console.log("darkMode in App", darkMode);
+	// console.log("dark-mode in app", window.localStorage.getItem("dark-mode"));
 
 	const appDiv = document.getElementById("app-container");
 
+	// useEffect(() => {
+	// 	setEleId(appDiv);
+	// }, [appDiv]);
+
+	const toggle = document.getElementById("mode-toggle");
 	useEffect(() => {
-		setEleId(appDiv);
-	}, [appDiv]);
+		function enableDarkMode() {
+			appDiv.classList.add("light-mode");
+			localStorage.setItem("dark-mode", "enabled");
+		}
+
+		function disableDarkMode() {
+			appDiv.classList.remove("light-mode");
+			localStorage.setItem("dark-mode", "disabled");
+		}
+
+		if (toggle) {
+			toggle.addEventListener("click", (event) => {
+				darkMode = localStorage.getItem("dark-mode"); // update darkMode when clickced
+				if (darkMode === "disabled") {
+					enableDarkMode();
+				} else {
+					disableDarkMode();
+				}
+			});
+		}
+	}, [appDiv, toggle]);
 
 	return (
-		<div id="app-container" className={appClass}>
+		<div id="app-container">
 			<h1>let;s get this money</h1>
 			<NavBar
 				token={token}
@@ -52,13 +78,20 @@ function App() {
 				spotifyToken={spotifyToken}
 				setSpotifyToken={setSpotifyToken}
 			/>
-			<DarkModeToggle
+			{/* <DarkModeToggle
 				appClass={appClass}
 				setAppClass={setAppClass}
 				eleId={eleId}
 				setEleId={setEleId}
 				isDark={isDark}
 				setIsDark={setIsDark}
+			/> */}
+			<Toggle
+				checked={isDark}
+				onChange={({ target }) => setIsDark(target.checked)}
+				icons={{ checked: "🌙", unchecked: "🔆" }}
+				aria-label="Dark mode toggle"
+				id="mode-toggle"
 			/>
 		</div>
 	);
--- client/src/components/DarkModeToggle.jsx
+++ client/src/components/DarkModeToggle.jsx
@@ -13,42 +13,72 @@ export default function DarkModeToggle({
 	setIsDark,
 }) {
 	// const [isDark, setIsDark] = useState(true);
+	// let darkMode = isDark;
+
 	// const [isDark2, setIsDark2] = useState(true);
 
 	// useEffect(() => {
 	// 	localStorage.setItem("isDark", isDark);
 	// }, [isDark]);
 
+	// if (darkMode === "enabled") {
+	// 	enableDarkMode();
+	// }
+
 	useEffect(() => {
+		let darkMode = localStorage.getItem("dark-mode");
 		const toggle = document.getElementById("mode-toggle");
 
-		if (isDark && eleId) {
-			toggle.addEventListener("click", function handleClick() {
-				console.log("isDark in handleToggle 1", isDark);
-				eleId.classList.add("light-mode");
-				// localStorage.setItem("isDark", isDark);
-				setIsDark(!isDark);
-				localStorage.setItem("isDark", isDark);
-
-				// setIsDark(JSON.parse(localStorage.getItem("isDark")));
-			});
-		} else if (!isDark && eleId) {
-			toggle.addEventListener("click", function handleClick() {
-				console.log("isDark in handleToggle 2", isDark);
-
-				eleId.classList.remove("light-mode");
-				setIsDark(!isDark);
-				localStorage.setItem("isDark", isDark);
-
-				// localStorage.setItem("isDark", isDark);
-
-				// console.log("JSONparse", JSON.parse(localStorage.getItem("isDark")));
-
-				// setIsDark(JSON.parse(localStorage.getItem("isDark")));
-				// eleId.classList.add("");
-			});
+		function enableDarkMode() {
+			eleId.classList.add("light-mode");
+			localStorage.setItem("dark-mode", "enabled");
 		}
-	}, [eleId, isDark, setAppClass, setEleId]);
+
+		function disableDarkMode() {
+			eleId.classList.remove("light-mode");
+			localStorage.setItem("dark-mode", "disabled");
+		}
+
+		toggle.addEventListener("click", (event) => {
+			darkMode = localStorage.getItem("dark-mode"); // update darkMode when clickced
+			if (darkMode === "disabled") {
+				enableDarkMode();
+			} else {
+				disableDarkMode();
+			}
+		});
+	}, [eleId]);
+
+	// useEffect(() => {
+	// 	const toggle = document.getElementById("mode-toggle");
+
+	// 	if (isDark && eleId) {
+	// 		toggle.addEventListener("click", function handleClick() {
+	// 			console.log("isDark in handleToggle 1", isDark);
+	// 			eleId.classList.add("light-mode");
+	// 			// localStorage.setItem("isDark", isDark);
+	// 			setIsDark(!isDark);
+	// 			localStorage.setItem("isDark", isDark);
+
+	// 			// setIsDark(JSON.parse(localStorage.getItem("isDark")));
+	// 		});
+	// 	} else if (!isDark && eleId) {
+	// 		toggle.addEventListener("click", function handleClick() {
+	// 			console.log("isDark in handleToggle 2", isDark);
+
+	// 			eleId.classList.remove("light-mode");
+	// 			setIsDark(!isDark);
+	// 			localStorage.setItem("isDark", isDark);
+
+	// 			// localStorage.setItem("isDark", isDark);
+
+	// 			// console.log("JSONparse", JSON.parse(localStorage.getItem("isDark")));
+
+	// 			// setIsDark(JSON.parse(localStorage.getItem("isDark")));
+	// 			// eleId.classList.add("");
+	// 		});
+	// 	}
+	// }, [eleId, isDark, setAppClass, setEleId]);
 
 	return (
 		<Toggle
