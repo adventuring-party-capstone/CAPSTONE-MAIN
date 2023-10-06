acb199d6a7ba7809e199ab6065cfa6ddf6e8e8e1
Emily Cheng <echeng0123@gmail.com>
2023-10-06 14:34:46 -0400

    working sidebar

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -2,8 +2,10 @@ import { Sidenav, initTE } from "tw-elements";
 
 export default function SideNav() {
 	initTE({ Sidenav });
+
 	return (
 		<>
+			<div className="text-3xl font-bold underline">hello</div>
 			<nav
 				id="sidenav-1"
 				className="absolute left-0 top-0 z-[1035] h-full w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800"
@@ -163,7 +165,8 @@ export default function SideNav() {
 				</ul>
 			</nav>
 			<button
-				className="mt-10 inline-block rounded bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+				id="side"
+				className="position fixed mt-10 inline-block rounded bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
 				data-te-sidenav-toggle-ref
 				data-te-target="#sidenav-1"
 				aria-controls="#sidenav-1"
