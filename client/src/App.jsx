b01273235cebb0eb47de567c2c7b7e70c9962031
Emily Cheng <echeng0123@gmail.com>
2023-10-06 11:18:02 -0400

    working dark mode toggle with refresh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,9 +13,6 @@ function App() {
 	const [spotifyToken, setSpotifyToken] = useState(null);
 	const [token, setToken] = useState(null);
 	const [userId, setUserId] = useState(null);
-	// const [appClass, setAppClass] = useState("");
-	// const [eleId, setEleId] = useState("");
-	// const [isDark, setIsDark] = useState(true);
 	const [dark, setDark] = useState(
 		window.localStorage.getItem("dark-mode") === "true"
 	);
@@ -24,51 +21,7 @@ function App() {
 		setToken(window.localStorage.getItem("token"));
 		setUserId(window.localStorage.getItem("userId"));
 		setSpotifyToken(window.localStorage.getItem("spotifyToken"));
-		// setIsDark(window.localStorage.getItem("dark-mode"));
-		// setIsDark(window.localStorage.getItem("isDark"));
 	}, []);
-
-	// DARK MODE CODE
-	// let darkMode = localStorage.getItem("dark-mode");
-	// let darkMode = localStorage.getItem("isDark");
-
-	// console.log("isDark in App", isDark);
-	// console.log("darkMode in App", darkMode);
-	// console.log("dark-mode in app", window.localStorage.getItem("dark-mode"));
-
-	const appDiv = document.getElementById("app-container");
-
-	// useEffect(() => {
-	// 	setEleId(appDiv);
-	// }, [appDiv]);
-
-	// const toggle = document.getElementById("mode-toggle");
-	// useEffect(() => {
-	// 	function enableDarkMode() {
-	// 		appDiv.classList.add("light-mode");
-	// 		// localStorage.setItem("dark-mode", "enabled");
-	// 		localStorage.setItem("isDark", "true");
-	// 	}
-
-	// 	function disableDarkMode() {
-	// 		appDiv.classList.remove("light-mode");
-	// 		// localStorage.setItem("dark-mode", "disabled")
-	// 		setIsDark(JSON.parse(localStorage.setItem("isDark", "false")));
-	// 	}
-
-	// 	if (toggle) {
-	// 		toggle.addEventListener("click", (event) => {
-	// 			// darkMode = localStorage.getItem("isDark"); // update isDark when clicked
-	// 			if (isDark === false) {
-	// 				enableDarkMode();
-	// 			} else if (isDark) {
-	// 				disableDarkMode();
-	// 			}
-	// 		});
-	// 	}
-	// }, [appDiv, toggle]);
-
-	// TESTING NEW CODE
 
 	useEffect(() => {
 		localStorage.setItem("dark-mode", dark);
@@ -95,24 +48,10 @@ function App() {
 				spotifyToken={spotifyToken}
 				setSpotifyToken={setSpotifyToken}
 			/>
-			{/* <DarkModeToggle
-				appClass={appClass}
-				setAppClass={setAppClass}
-				eleId={eleId}
-				setEleId={setEleId}
-				isDark={isDark}
-				setIsDark={setIsDark}
-			/> */}
-			{/* <Toggle
-				checked={isDark}
-				onChange={({ target }) => setIsDark(target.checked)}
-				icons={{ checked: "🌙", unchecked: "🔆" }}
-				aria-label="Dark mode toggle"
-				id="mode-toggle"
-			/> */}
-			<button className="dark-mode-toggle" onClick={toggleDarkMode}>
-				Enable Dark Mode
-			</button>
+			<label className="switch">
+				<input type="checkbox" onClick={toggleDarkMode} />
+				<span className="slider round">Dark Mode</span>
+			</label>
 		</div>
 	);
 }
