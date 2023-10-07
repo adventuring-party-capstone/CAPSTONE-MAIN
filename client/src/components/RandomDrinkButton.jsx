5813fa5429892ff39b944ff82aab6c7fe6da3c09
winniemei <wmei91@gmail.com>
2023-10-07 07:50:00 -0400

    get random drink to flip

--- client/src/components/RandomDrinkButton.jsx
+++ client/src/components/RandomDrinkButton.jsx
@@ -26,22 +26,33 @@ export default function RandomDrinkButton({ userId }) {
 			<div className="Random-Button">
 				{randomDrink ? (
 					<div>
-						{randomDrink && <img src={randomDrink.strDrinkThumb} />}
-						{randomDrink && <h1>{randomDrink.strDrink}</h1>}
-						{randomDrink.strDrinkThumb && (
-							<DetailsButton drinkId={randomDrink.idDrink} />
-						)}
-						{randomDrink.strDrinkThumb && (
-							<FavoriteButton
-								userId={userId}
-								api_drinks_id={randomDrink.idDrink}
-							/>
-						)}
+						<div id="flip-card">
+							<div id="flip-card-inner">
+								<div id="flip-card-front">
+									{randomDrink && <h1>{randomDrink.strDrink}</h1>}
+									{randomDrink && <img src={randomDrink.strDrinkThumb} />}
+								</div>
+								<div id="flip-card-back">
+
+									{randomDrink && <h1>{randomDrink.strDrink}</h1>}
+									{randomDrink.strDrinkThumb && (
+										<DetailsButton drinkId={randomDrink.idDrink} />
+									)}
+									{randomDrink.strDrinkThumb && (
+										<FavoriteButton
+											userId={userId}
+											api_drinks_id={randomDrink.idDrink}
+										/>
+									)}
+								</div>
+							</div>
+						</div>
 					</div>
+
 				) : (
 					<></>
 				)}
-			</div>
+			</div >
 		</>
 	);
 }
