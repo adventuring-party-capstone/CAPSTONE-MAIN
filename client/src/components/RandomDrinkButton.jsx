5a86a98d40ea7f1d0e4a74406705e24ad20d81d4
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-10-09 15:37:29 -0400

    alc icons work on random drinks

--- client/src/components/RandomDrinkButton.jsx
+++ client/src/components/RandomDrinkButton.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import { fetchRandomDrink } from "../../fetching/randomdrink";
 import DetailsButton from "./DetailsButton";
 import FavoriteButton from "./FavoriteButton";
+import { fetchAllAlcDrinks } from "../../fetching/cocktaildb";
 
 export default function RandomDrinkButton({ userId }) {
      const [randomDrink, setRandomDrink] = useState(null);
+     const [alcArray, setAlcArray] = useState([]);
+     const [alcIds, setAlcIds] = useState([]);
+
      const getRandomDrink = async () => {
           const response = await fetchRandomDrink();
           console.log(
@@ -20,6 +24,28 @@ export default function RandomDrinkButton({ userId }) {
      function handleClick() {
           getRandomDrink();
      }
+
+     //getting all alcoholic drinks from the api
+     useEffect(() => {
+          async function getAlcoholicDrinks() {
+               const alcDrinks = await fetchAllAlcDrinks();
+               console.log("alcDrinks.drinks line 32", alcDrinks.drinks);
+               setAlcArray(alcDrinks.drinks);
+          }
+          getAlcoholicDrinks();
+          console.log("alcArray in 35", alcArray);
+     }, []);
+
+     //pushing the ids from alcoholic drinks into an array
+     const alcIdArray = [];
+     useEffect(() => {
+          for (let i = 0; i < alcArray.length; i++) {
+               alcIdArray.push(alcArray[i].idDrink);
+          }
+          console.log("alcIdArray inside UE", alcIdArray);
+          setAlcIds(alcIdArray);
+     }, [alcArray]);
+
      //   KEEP THIS
      console.log("is this from my random drink component?", randomDrink);
      return (
@@ -31,11 +57,24 @@ export default function RandomDrinkButton({ userId }) {
                               <div id="flip-card">
                                    <div id="flip-card-inner">
                                         <div id="flip-card-front">
-                                             {randomDrink && (
-                                                  <h1>
-                                                       {randomDrink.strDrink}
-                                                  </h1>
-                                             )}
+                                             <div id="name section">
+                                                  {alcIds.includes(
+                                                       randomDrink.idDrink
+                                                  ) ? (
+                                                       <p>
+                                                            🍸
+                                                            {
+                                                                 randomDrink.strDrink
+                                                            }
+                                                       </p>
+                                                  ) : (
+                                                       <p>
+                                                            {
+                                                                 randomDrink.strDrink
+                                                            }
+                                                       </p>
+                                                  )}
+                                             </div>
                                              {randomDrink && (
                                                   <img
                                                        src={
