beeff3b3fa30621d58a5505578fb459103df6869
Annalisa Boerner <annalisa.boerner@gmail.com>
2023-10-09 20:57:29 -0400

    functional delete button on userCreatedDrinks

---
+++ client/src/components/DeleteUserCreatedDrink.jsx
@@ -0,0 +1,27 @@
+// This component handles the delete function for a drink that the user created
+
+import { deleteDrink } from "../../fetching/local";
+import { useNavigate } from "react-router-dom";
+
+export default function DeleteUserCreatedDrink({ drinks_id }) {
+     const nav = useNavigate();
+     async function handleSubmit(event) {
+          event.preventDefault();
+          try {
+               drinks_id && (await deleteDrink(drinks_id));
+               nav(0);
+          } catch (error) {
+               console.error(error);
+          }
+     }
+     return (
+          <div>
+               <button
+                    onClick={handleSubmit}
+                    id="delete-user-created-drink-button"
+               >
+                    Delete Drink
+               </button>
+          </div>
+     );
+}
