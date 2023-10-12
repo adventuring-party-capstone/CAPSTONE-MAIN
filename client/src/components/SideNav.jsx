6c144500f17549fec6d4df74c07fc85cb323ee21
winniemei <wmei91@gmail.com>
2023-10-12 16:29:35 -0400

    fancy burger 1

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -4,14 +4,23 @@ import { AiOutlineLogout, AiOutlineLogin } from "react-icons/ai";
 import { BiSolidDrink, BiDrink } from "react-icons/bi";
 
 export default function SideNav({ mode, token }) {
-	const [isOpen, setIsOpen] = useState(false);
 	const buttonX = document.getElementById("buttonX");
 
-	function toggleNav() {
-		if (isOpen) {
-			myFunction(buttonX)
-		}
-	}
+	// function toggleNav() {
+	// 	if (document.getElementsByClassName("container change")) {
+	// 		toggleSideBar();
+	// 	}
+	// }
+
+	// function toggleSideBar() {
+	// 	if (document.getElementById("mySidenav")) {
+	// 		document.getElementById("mySidenav").style.width = "250px";
+	// 		myFunction(buttonX);
+	// 	}
+	// 	else {
+	// 		document.getElementById("mySidenav").style.width = "0";
+	// 	}
+	// }
 
 	function myFunction(x) {
 		x.classList.toggle("change");
@@ -32,11 +41,20 @@ export default function SideNav({ mode, token }) {
 		// }
 	}
 
+	const closedContainer = document.getElementsByClassName("container");
+	const openedContainer = document.getElementsByClassName("container change");
 
+	function handleNavChanges() {
+		if (openedContainer) {
+			openedContainer?.addEventListener("click", closeNav());
+		} else if (closedContainer) {
+			closedContainer?.addEventListener("click", openNav());
+		}
+	}
 
 	return (
 		<div>
-			<div id="buttonX" className="container" onClick={() => openNav()}>
+			<div id="buttonX" className="container" onClick={() => handleNavChanges()}>
 				<div className="bar1"></div>
 				<div className="bar2"></div>
 				<div className="bar3"></div>
