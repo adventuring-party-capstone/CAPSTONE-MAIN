337f300369de442b1a958ce95b0426b124403644
Emily Cheng <echeng0123@gmail.com>
2023-10-12 18:02:45 -0400

    working with set states

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -1,73 +1,45 @@
 import { useState, useEffect } from "react";
-import { BsMusicNoteBeamed } from "react-icons/bs";
-import { AiOutlineLogout, AiOutlineLogin } from "react-icons/ai";
+import { BsMusicNoteBeamed, BsTypeH1 } from "react-icons/bs";
+import {
+	AiOutlineLogout,
+	AiOutlineLogin,
+	AiOutlineConsoleSql,
+} from "react-icons/ai";
 import { BiSolidDrink, BiDrink } from "react-icons/bi";
 
 export default function SideNav({ mode, token }) {
 	const buttonX = document.getElementById("buttonX");
-
-	// function toggleNav() {
-	// 	if (document.getElementsByClassName("container change")) {
-	// 		toggleSideBar();
-	// 	}
-	// }
-
-	// function toggleSideBar() {
-	// 	if (document.getElementById("mySidenav")) {
-	// 		document.getElementById("mySidenav").style.width = "250px";
-	// 		myFunction(buttonX);
-	// 	}
-	// 	else {
-	// 		document.getElementById("mySidenav").style.width = "0";
-	// 	}
-	// }
+	const [isOpen, setIsOpen] = useState(true);
 
 	function myFunction(x) {
 		x.classList.toggle("change");
-	}
-
-	/* Set the width of the side navigation to 250px */
-	function openNav() {
-		if (document.getElementById("mySidenav")) {
-			document.getElementById("mySidenav").style.width = "250px";
-			myFunction(buttonX);
-		}
-	}
-
-	// /* Set the width of the side navigation to 0 */
-	function closeNav() {
-		// if (document.getElementById("mySidenav")?.width !== "0") {
-		document.getElementById("mySidenav").style.width = "0";
-		// }
-	}
-
-	const closedContainer = document.getElementsByClassName("container");
-	const openedContainer = document.getElementsByClassName("container change");
-
-	function handleNavChanges() {
-		if (openedContainer) {
-			openedContainer?.addEventListener("click", closeNav());
-		} else if (closedContainer) {
-			closedContainer?.addEventListener("click", openNav());
-		}
+		setIsOpen(!isOpen);
 	}
 
 	return (
 		<div>
-			<div id="buttonX" className="container" onClick={() => handleNavChanges()}>
+			<div
+				id="buttonX"
+				className="container"
+				onClick={() => myFunction(buttonX)}
+				// onClick={() => myFunction(buttonX)}
+			>
 				<div className="bar1"></div>
 				<div className="bar2"></div>
 				<div className="bar3"></div>
 			</div>
+			{isOpen && <h1>HI</h1>}
 
-			<div id="mySidenav" className="sidenav">
-
-				<a href="" className="closebtn" onClick={() => closeNav()}>&times;</a>
-				<a href="/">Home</a>
-				<a href="/all_drinks">Drinks</a>
-				<a href="#">Clients</a>
-				<a href="#">Contact</a>
-			</div>
+			{!isOpen && (
+				<div>
+					<div id="mySidenav" className="sidenav">
+						<a href="/">Home</a>
+						<a href="/all_drinks">Drinks</a>
+						<a href="#">Clients</a>
+						<a href="#">Contact</a>
+					</div>
+				</div>
+			)}
 		</div>
-	)
+	);
 }
