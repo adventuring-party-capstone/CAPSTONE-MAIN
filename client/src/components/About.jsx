c700e55bc060b64e0aff7a285d47648a746abb85
shaberry <jaden.png02@gmail.com>
2023-10-13 14:27:22 -0700

    about new additions

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,95 +1,96 @@
 export default function About() {
     console.log("about usss");
     return (
-        <div className="aboutus">
-            <img
-                src="../../src/assets/Studio_drink_logo.svg"
-                className="aboutlogo"
-            />
-            <h1>The StudioDrink team:</h1>
-            <h2>what our app is about</h2>
-            <img src="../src/assets/divider3.png" className="divider" />
-            <br />
-            {/* PROFILE ONE */}
-            <div className="Individuals">
-                <div className="picture">
-                    <img src="../src/assets/Hali.PNG" className="pfp" />
-                    <h2>Hali Jaden Rodriguez (She/Her)</h2>
-                    {/* spotify favorit song */}
-                    <iframe
-                        className="spotty"
-                        src="https://open.spotify.com/embed/track/3vkCueOmm7xQDoJ17W1Pm3?utm_source=generator"
-                        height="152"
-                        allowfullscreen=""
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                        loading="lazy"
-                    ></iframe>
-                </div>
+        <section>
+            <div className="aboutus">
+                <img
+                    src="../../src/assets/Studio_drink_logo.svg"
+                    className="aboutlogo"
+                />
+                <h1>The StudioDrink team:</h1>
+                <h2>what our app is about</h2>
+                <img src="../src/assets/divider3.png" className="divider" />
                 <br />
-                <div className="aboutsec">
-                    <h3>
-                        I am Hali. I am a 21 year old Fullstack developer and
-                        artist!.
-                    </h3>
-                </div>
-
-                {/* CONTACT LINKS */}
-                <div className="contacts">
-                    {/* github link */}
-                    <a href="https://github.com/Shaberryy">
-                        <img
-                            src="https://static-00.iconduck.com/assets.00/github-inverted-icon-512x499-xwqq0y67.png"
-                            className="contactLinks"
-                        />
-                    </a>
-                    {/* linkedIn link */}
-                    <a href="https://www.linkedin.com/in/hali-rodriguez/">
-                        <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"
-                            className="contactLinks"
-                        />
-                    </a>
-                    {/* add portfolio link */}
-                    <a href="">
-                        <img
-                            src="https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Folder-2-512.png"
-                            className="contactLinks"
-                        />
-                    </a>
-                </div>
-            </div>
-            <br />
-            {/* PROFILE TWO */}
-            <div className="Individuals">
-                {/* CONTACT LINKS */}
-                <div className="contacts">
+                {/* PROFILE ONE */}
+                <div className="Individuals">
+                    <div className="picture">
+                        <img src="../src/assets/Hali.PNG" className="pfp" />
+                        <h2>Hali Jaden Rodriguez (She/Her)</h2>
+                        {/* spotify favorit song */}
+                        <iframe
+                            className="spotty"
+                            src="https://open.spotify.com/embed/track/3vkCueOmm7xQDoJ17W1Pm3?utm_source=generator"
+                            height="152"
+                            allowfullscreen=""
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"
+                        ></iframe>
+                    </div>
                     <br />
-                    {/* github link */}
-                    <a href="https://github.com/Shaberryy">
-                        <img
-                            src="https://static-00.iconduck.com/assets.00/github-inverted-icon-512x499-xwqq0y67.png"
-                            className="contactLinks"
-                        />
-                    </a>
-                    {/* linkedIn link */}
-                    <a href="https://github.com/Shaberryy">
-                        <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"
-                            className="contactLinks"
-                        />
-                    </a>
-                    {/* add portfolio link */}
-                    <a href="">
-                        <img
-                            src="https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Folder-2-512.png"
-                            className="contactLinks"
-                        />
-                    </a>
+                    <div className="aboutsec">
+                        <h3>
+                            I am Hali. I am a 21 year old Fullstack developer
+                            and artist!.
+                        </h3>
+                    </div>
+
+                    {/* CONTACT LINKS */}
+                    <div className="contacts">
+                        {/* github link */}
+                        <a href="https://github.com/Shaberryy">
+                            <img
+                                src="https://static-00.iconduck.com/assets.00/github-inverted-icon-512x499-xwqq0y67.png"
+                                className="contactLinks"
+                            />
+                        </a>
+                        {/* linkedIn link */}
+                        <a href="https://www.linkedin.com/in/hali-rodriguez/">
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"
+                                className="contactLinks"
+                            />
+                        </a>
+                        {/* add portfolio link */}
+                        <a href="">
+                            <img
+                                src="https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Folder-2-512.png"
+                                className="contactLinks"
+                            />
+                        </a>
+                    </div>
                 </div>
                 <br />
-                <div className="aboutsec">
-                    <h3>Once upon a time there was a lovely princess.</h3>
-                </div>
+                {/* PROFILE TWO */}
+                <div className="Individuals">
+                    {/* CONTACT LINKS */}
+                    <div className="contacts">
+                        <br />
+                        {/* github link */}
+                        <a href="https://github.com/Shaberryy">
+                            <img
+                                src="https://static-00.iconduck.com/assets.00/github-inverted-icon-512x499-xwqq0y67.png"
+                                className="contactLinks"
+                            />
+                        </a>
+                        {/* linkedIn link */}
+                        <a href="https://github.com/Shaberryy">
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"
+                                className="contactLinks"
+                            />
+                        </a>
+                        {/* add portfolio link */}
+                        <a href="">
+                            <img
+                                src="https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Folder-2-512.png"
+                                className="contactLinks"
+                            />
+                        </a>
+                    </div>
+                    <br />
+                    <div className="aboutsec">
+                        <h3>Once upon a time there was a lovely princess.</h3>
+                    </div>
 
                     <div className="picture">
                         <img
