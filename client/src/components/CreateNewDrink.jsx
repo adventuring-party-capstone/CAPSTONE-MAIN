1e2634cce043be59d0ad3f6854e8edf0edd4a86d
winniemei <wmei91@gmail.com>
2023-10-13 14:42:17 -0400

    mess around w mixologist

--- client/src/components/CreateNewDrink.jsx
+++ client/src/components/CreateNewDrink.jsx
@@ -9,167 +9,163 @@ import Grow from "@mui/material/Grow";
 import MuiAlert from "@mui/material/Alert";
 
 function SlideTransition(props) {
-	return <Slide {...props} direction="up" />;
+    return <Slide {...props} direction="up" />;
 }
 
 function GrowTransition(props) {
-	return <Grow {...props} />;
+    return <Grow {...props} />;
 }
 
 export default function CreateNewDrink({ token, userId }) {
-	const [drinksName, setDrinksName] = useState("");
-	const [ingredients, setIngredients] = useState("");
-	const [recipe, setRecipe] = useState("");
-	const [image, setImage] = useState("");
-	const [glass, setGlass] = useState("");
-	const [alcoholic, setAlcoholic] = useState("");
-	const [state, setState] = React.useState({
-		open: false,
-		Transition: Fade,
-	});
-	const navigate = useNavigate();
+    const [drinksName, setDrinksName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [recipe, setRecipe] = useState("");
+    const [image, setImage] = useState("");
+    const [glass, setGlass] = useState("");
+    const [alcoholic, setAlcoholic] = useState("");
+    const [state, setState] = React.useState({
+        open: false,
+        Transition: Fade,
+    });
+    const navigate = useNavigate();
 
-	if (!token) {
-		navigate("/login");
-	}
+    if (!token) {
+        navigate("/login");
+    }
 
-	const handleClick = (Transition) => () => {
-		setState({
-			open: true,
-			Transition,
-		});
-	};
+    const handleClick = (Transition) => () => {
+        setState({
+            open: true,
+            Transition,
+        });
+    };
 
-	const handleClose = () => {
-		setState({
-			...state,
-			open: false,
-		});
-	};
+    const handleClose = () => {
+        setState({
+            ...state,
+            open: false,
+        });
+    };
 
-	// console.log("userId in createnewdrink", userId);
-	const userIdCND = Number(userId);
-	// console.log("userIdCND", userIdCND);
+    // console.log("userId in createnewdrink", userId);
+    const userIdCND = Number(userId);
+    // console.log("userIdCND", userIdCND);
 
-	async function handleSubmit(e) {
-		try {
-			e.preventDefault();
-			console.log("..entering create drink handle submit");
-			const createDrinkResult = await createDrink(
-				null,
-				drinksName,
-				ingredients,
-				recipe,
-				image,
-				null,
-				alcoholic,
-				userIdCND
-			);
-			console.log("API Data", createDrinkResult);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+    async function handleSubmit(e) {
+        try {
+            e.preventDefault();
+            console.log("..entering create drink handle submit");
+            const createDrinkResult = await createDrink(
+                null,
+                drinksName,
+                ingredients,
+                recipe,
+                image,
+                null,
+                alcoholic,
+                userIdCND
+            );
+            console.log("API Data", createDrinkResult);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-	const handleChange = (e) => {
-		setAlcoholic(e.target.value);
-	};
+    const handleChange = (e) => {
+        setAlcoholic(e.target.value);
+    };
 
-	return (
-		<div className="formGroup">
-			<h1>Create Your Own Drink</h1>
-			<img
-				id="drink-gif"
-				src="https://i.pinimg.com/originals/1f/b3/8b/1fb38bda65bcc8600e048be51e4304a1.gif"
-			></img>
-			<br />
-			<form id="drink-form">
-				<input
-					id="formInput"
-					className="inputField"
-					value={drinksName}
-					type="text"
-					name="title"
-					placeholder="Drink name"
-					onChange={(e) => setDrinksName(e.target.value)}
-				/>
-				<br />
-				<input
-					id="formInput"
-					className="inputField"
-					value={image}
-					type="text"
-					name="title"
-					placeholder="Image"
-					onChange={(e) => setImage(e.target.value)}
-				/>
-				<br />
-				<textarea
-					cols={50}
-					rows={10}
-					id="formInput"
-					className="inputField"
-					value={ingredients}
-					type="text"
-					name="ingredients"
-					placeholder="Ingredients"
-					onChange={(e) => setIngredients(e.target.value)}
-				/>
-				<br />
-				<textarea
-					cols={50}
-					rows={10}
-					id="formInput"
-					className="inputField"
-					value={recipe}
-					type="text"
-					name="recipe"
-					placeholder="Recipe"
-					onChange={(e) => setRecipe(e.target.value)}
-				/>
-				<br />
-				<br />
-				<div>
-					<h2>Alcoholic?</h2>
-				</div>
-				<Select
-					id="formInput"
-					className="inputField"
-					value={alcoholic}
-					type="text"
-					name="alcoholic"
-					placeholder="alcoholic"
-					onChange={handleChange}
-				>
-					<MenuItem value={false}>No</MenuItem>
-					<MenuItem value={true}>Yes</MenuItem>
-				</Select>
-				<br />
-				<br />
-				<button
-					onClick={(e) => {
-						{
-							handleSubmit(e);
-							handleClick(GrowTransition)();
-						}
-					}}
-					id="pink-button"
-				>
-					Submit
-				</button>
-			</form>
-			<div>
-				{/* <Button onClick={handleClick(GrowTransition)}>Grow Transition</Button>
+    return (
+        <div className="formGroup">
+            <h1>Create Your Own Drink</h1>
+            <br />
+            <form id="drink-form">
+                <input
+                    id="formInput"
+                    className="inputField"
+                    value={drinksName}
+                    type="text"
+                    name="title"
+                    placeholder="Drink name"
+                    onChange={(e) => setDrinksName(e.target.value)}
+                />
+                <br />
+                <input
+                    id="formInput"
+                    className="inputField"
+                    value={image}
+                    type="text"
+                    name="title"
+                    placeholder="Image"
+                    onChange={(e) => setImage(e.target.value)}
+                />
+                <br />
+                <textarea
+                    cols={50}
+                    rows={10}
+                    id="formInput"
+                    className="inputField"
+                    value={ingredients}
+                    type="text"
+                    name="ingredients"
+                    placeholder="Ingredients"
+                    onChange={(e) => setIngredients(e.target.value)}
+                />
+                <br />
+                <textarea
+                    cols={50}
+                    rows={10}
+                    id="formInput"
+                    className="inputField"
+                    value={recipe}
+                    type="text"
+                    name="recipe"
+                    placeholder="Recipe"
+                    onChange={(e) => setRecipe(e.target.value)}
+                />
+                <br />
+                <br />
+                <div>
+                    <h2>Alcoholic?</h2>
+                </div>
+                <Select
+                    id="formInput"
+                    className="inputField"
+                    value={alcoholic}
+                    type="text"
+                    name="alcoholic"
+                    placeholder="alcoholic"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={false}>No</MenuItem>
+                    <MenuItem value={true}>Yes</MenuItem>
+                </Select>
+                <br />
+                <br />
+                <button
+                    onClick={(e) => {
+                        {
+                            handleSubmit(e);
+                            handleClick(GrowTransition)();
+                        }
+                    }}
+                    id="pink-button"
+                >
+                    Submit
+                </button>
+            </form>
+            <div>
+                {/* <Button onClick={handleClick(GrowTransition)}>Grow Transition</Button>
 				<Button onClick={handleClick(Fade)}>Fade Transition</Button>
 				<Button onClick={handleClick(SlideTransition)}>Slide Transition</Button> */}
-				<Snackbar
-					open={state.open}
-					onClose={handleClose}
-					TransitionComponent={state.Transition}
-					message="Congrats! You created a drink!"
-					key={state.Transition.name}
-				/>
-			</div>
-		</div>
-	);
+                <Snackbar
+                    open={state.open}
+                    onClose={handleClose}
+                    TransitionComponent={state.Transition}
+                    message="Congrats! You created a drink!"
+                    key={state.Transition.name}
+                />
+            </div>
+        </div>
+    );
 }
