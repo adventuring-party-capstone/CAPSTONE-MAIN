add3b1dfc8c95716ab834dfb4324a0ced89c42fc
Emily Cheng <echeng0123@gmail.com>
2023-10-16 12:23:38 -0400

    fixing deployment issues

--- server/db/client.js
+++ server/db/client.js
@@ -6,8 +6,9 @@ const { Client } = require("pg");
 
 // const studio = "studiodrink";
 // const client = new Client(`postgres://localhost:5432/${studio}`);
+// `postgres://studiodrinkbeta2_user:bsNa8DiK8ToOFbEFe7I7g4Fwn5Dnhd0z@dpg-ckja99a12bvs7391712g-a/studiodrinkbeta2`
 const client = new Client(
-	`postgres://studiodrinkbeta2_user:bsNa8DiK8ToOFbEFe7I7g4Fwn5Dnhd0z@dpg-ckja99a12bvs7391712g-a/studiodrinkbeta2`
+	`postgres://studiodrinkwestdb_user:Nf9AX3FNjnFPtbgEZDYrueS4MV8IOKqb@dpg-ckmm6f2v7m0s73b0u2s0-a/studiodrinkwestdb`
 );
 
 // export for use in other files
